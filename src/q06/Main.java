71b09f706802bd4ce8a63eded7e629676fe9c1d3
Shreyagupta9670 <shreyaguptasg9670@gmail.com>
2026-04-08 11:49:25 +0530

    Add Fibonacci sequence printing in Main.java
    Implemented Fibonacci sequence printing for first n numbers.

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,15 +1,27 @@
-import java.util.*;
+ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
+        int a = 0, b = 1;
 
+        for (int i = 0; i < n; i++) {
+            // print number
+            System.out.print(a);
+
+            // avoid trailing space
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+
+            // next fibonacci
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        sc.close();
     }
 }
