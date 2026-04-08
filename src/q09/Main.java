02b48b4e8761931d38d18ddede0be64a2d6b2af6
Shreyagupta9670 <shreyaguptasg9670@gmail.com>
2026-04-08 13:02:24 +0530

    Add stack operations to Main.java
    Implemented stack operations including PUSH, POP, PEEK, and SIZE.

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,24 +1,41 @@
-import java.util.*;
+ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Read two n×n matrices A and B, multiply them → print result C = A×B
-        //       Input: matrix A row by row, then matrix B row by row
-        //       Output: result matrix row by row, values separated by spaces
-        //
-        // Input:
-        // 2
-        // 1 2
-        // 3 4
-        // 5 6
-        // 7 8
-        //
-        // Output:
-        // 19 22
-        // 43 50
+        int[] stack = new int[n]; // max size n
+        int top = -1;
 
+        for (int i = 0; i < n; i++) {
+            String op = sc.next();
+
+            if (op.equals("PUSH")) {
+                int x = sc.nextInt();
+                if (top < n - 1) {
+                    stack[++top] = x;
+                }
+
+            } else if (op.equals("POP")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top--]);
+                }
+
+            } else if (op.equals("PEEK")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                }
+
+            } else if (op.equals("SIZE")) {
+                System.out.println(top + 1);
+            }
+        }
+
+        sc.close();
     }
 }
